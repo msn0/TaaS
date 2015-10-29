d4020f6517a45d1be42992ac403491a803bba1f3
Jerzy Jelinek <jerzy.jelinek@allegro.pl>
2015-10-29 15:39:34 +0100

    add editorconfig

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ port: 9000 });
+var wss = new WebSocketServer({port: 9000});
 var socket;
 
-wss.on('connection', function connection (ws) {
+wss.on('connection', function connection(ws) {
   socket = ws;
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.render('client');
 });
 
-app.get('/api/toilets/:gender/:id', function(req, res, next){
+app.get('/api/toilets/:gender/:id', function (req, res, next) {
   var event = {
     gender: req.params.gender,
     action: req.query.action,
